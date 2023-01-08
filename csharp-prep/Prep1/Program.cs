b027da4d99150bff95bea589724287705b935cad
Alanpea05 <alan.pear05@gmail.com>
2023-01-07 18:36:45 -0700

    Activities_W01

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,15 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Wht is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine(" ");
+        Console.WriteLine($"Your name is {name}, {name} {last_name}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade (Percentage)? ");
+        string grade = Console.ReadLine(); 
+        int grade_n = int.Parse(grade);
+
+        string letter = "";
+        
+        if (grade_n >= 90) {
+            letter = ("A!");
+        }
+        else if (grade_n >= 80){
+            letter = ("B!");
+        }
+        else if (grade_n >= 70){
+            letter = ("C!");
+        }
+        else if (grade_n >= 60){
+            letter = ("D!");
+        }
+        else{
+            letter = ("F");
+        }
+        Console.WriteLine("");
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade_n >= 70){
+            Console.WriteLine("Congratulation! You passed the course");
+        }
+        else {
+            Console.WriteLine("Sorry, you can do better, try a little harder next time! ");
+        }
+        Console.WriteLine("");
+
     }
 }
